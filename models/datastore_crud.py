a6c1b54364d16f064c1ff71007fb58f98b69f12e
Otazoman <ujimasa@hotmail.com>
2020-07-26 16:15:34 +0900

    2020-07-26 Module division and refactoring

---
+++ models/datastore_crud.py
@@ -0,0 +1,78 @@
+import sys
+import traceback
+
+from google.cloud import datastore
+
+class DataStoreOperate():
+    def __init__(self,client=None):
+        self.client = datastore.Client()
+    def insert_datastore(self,kind_name,insert_data):
+        """ Insert DataStore"""
+        try:
+            key = self.client.key(kind_name)
+            entity = datastore.Entity(key)
+            entity.update(insert_data)
+            result = self.client.put(entity)
+            return result
+        except Exception as e:
+            print('Error Insert table:' + kind_name )
+            t, v, tb = sys.exc_info()
+            print(traceback.format_exception(t,v,tb))
+            print(traceback.format_tb(e.__traceback__))
+            return False
+    def search_datastore(self,kind_name,search):
+        """ Search DataStore"""
+        try:
+            query = self.client.query(kind=kind_name)
+            keylist= [i for i in search.keys()]
+            valuelist = [ j for j in search.values()]
+            for k,v in zip(keylist,valuelist):
+                query.add_filter(k, "=", v )
+            result = list(query.fetch())
+            return result
+        except Exception as e:
+            print('Error Search Data')
+            t, v, tb = sys.exc_info()
+            print(traceback.format_exception(t,v,tb))
+            print(traceback.format_tb(e.__traceback__))
+            return False
+    def get_datastore_id(self,searchresult):
+        """ Get Datastore Record Key Id """
+        try:
+            result = []
+            for entity in searchresult:   
+                gid = entity.id
+                result.append(gid)
+            return result
+        except Exception as e:
+            print('Error Get ID')
+            t, v, tb = sys.exc_info()
+            print(traceback.format_exception(t,v,tb))
+            print(traceback.format_tb(e.__traceback__))
+            return False
+    def update_datastore(self,kind_name,target_id,prop):
+        """ Update Datastore  """
+        try:
+            key = self.client.key(kind_name,target_id)
+            data = self.client.get(key)
+            data.update(prop)
+            result = self.client.put(data)
+            return result
+        except Exception as e:
+            print('Error Update Kind:'+kind_name)
+            t, v, tb = sys.exc_info()
+            print(traceback.format_exception(t,v,tb))
+            print(traceback.format_tb(e.__traceback__))
+            return False
+    def delete_datastore(self,kind_name,target_id):
+        """ Delete Datastore  """
+        try:
+            key = self.client.key(kind_name,target_id)        
+            result = self.client.delete(key)
+            return result
+        except Exception as e:
+            print('Error Delete Kind:'+kind_name)
+            t, v, tb = sys.exc_info()
+            print(traceback.format_exception(t,v,tb))
+            print(traceback.format_tb(e.__traceback__))
+            return False
---
+++ models/sample.py
@@ -0,0 +1,62 @@
+import sys
+import traceback
+from datastore_crud  import DataStoreOperate
+
+def main():
+    try:
+        ds_crud =  DataStoreOperate()
+
+        keyname = "Tasks"
+
+        datas = []
+        i = 0
+        while i < 5:
+            data = dict()
+            data['test_key1'] = 'value' + str(i)
+            data['test_key2'] = 'value' + str(i)
+            data['test_key3'] = 'value' + str(i)
+            datas.append(data)
+            i += 1
+        
+        # Insert
+        for rec in datas:
+            ds_crud.insert_datastore(keyname,rec)
+
+        # Search
+        search = {
+                    'test_key1':'value2'
+        }
+        searchresult = ds_crud.search_datastore(keyname,search)
+        gid = ds_crud.get_datastore_id(searchresult)
+        for s in searchresult:
+            print(s)
+        
+        # Update
+        updatekey ={
+                    'test_key1':'value3'
+        }
+        update_value ={
+                    'test_key1':'AAA1',
+                    'test_key2':'BBB2',
+                    'test_key3':'CCC3',
+                    'test_key4':'Done'
+        }
+        preud = ds_crud.search_datastore(keyname,updatekey)
+        uid = ds_crud.get_datastore_id(preud)
+        for u in uid:
+            ds_crud.update_datastore(keyname,u,update_value)
+
+        # Delete
+        for d in gid:
+            rtn = ds_crud.delete_datastore(keyname,d)
+
+        print('Done')
+    except Exception as e:
+        print('Error')
+        t, v, tb = sys.exc_info()
+        print(traceback.format_exception(t,v,tb))
+        print(traceback.format_tb(e.__traceback__))
+
+
+if __name__ == '__main__':
+   main()
